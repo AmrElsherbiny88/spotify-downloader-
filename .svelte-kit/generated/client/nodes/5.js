335672847ce3558fdf0f682e5d80e5317b10e0e0
AmrElsherbiny <amoryelsherbiny@gmail.com>
2024-12-12 13:31:07 +0200

    project init

---
+++ .svelte-kit/generated/client/nodes/5.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/Themes/+page.svelte";
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import '../app.postcss';
+
+	let { children } = $props();
+$effect(() => {
+  AOS.init({
+	duration: 1750, // Animation duration
+	
+  });
+  AOS.refresh(); // Ensures AOS is aware of any new content in the component
+});
+</script>
+
+
+
+	<div  class="flex flex-col justify-items-center text-center justify-center">
+		{@render children()}
+	</div>
